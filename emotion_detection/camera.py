e643debaffb0b93939f877290f73b2544e88dcef
TommyTao1126 <143057605+TommyTao1126@users.noreply.github.com>
2024-02-17 17:14:09 -0500

    Upload Emotion Detection

---
+++ emotion_detection/camera.py
@@ -0,0 +1,38 @@
+'''
+Description: 
+Logo:                                                                      ▄   ▄
+░██████╗██╗░░██╗░█████╗░░██╗░░░░░░░██╗███╗░░██╗  ░█████╗░░█████╗░██╗ █▀█▀█
+██╔════╝██║░░██║██╔══██╗░██║░░██╗░░██║████╗░██║  ██╔══██╗██╔══██╗██║ █▄█▄█
+╚█████╗░███████║███████║░╚██╗████╗██╔╝██╔██╗██║  ██║░░╚═╝███████║██║ ███  ▄▄
+░╚═══██╗██╔══██║██╔══██║░░████╔═████║░██║╚████║  ██║░░██╗██╔══██║██║ ████▐█ █
+██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░██║░╚███║  ╚█████╔╝██║░░██║██║ ████   █
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░░╚══╝  ░╚════╝░╚═╝░░╚═╝╚═╝ ▀▀▀▀▀▀▀
+Author: Shawn C
+Copyright (c) 2022 by Shawn C., All Rights Reserved. 
+'''
+import cv2
+from model import FacialExpressionModel
+import numpy as np
+
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+model = FacialExpressionModel("model.json", "model_weights.h5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        # video or image link should also apply
+    def __del__(self):
+        self.video.release()
+
+    # returns camera frames along with bounding boxes and predictions
+    def get_frame(self):
+        _, fr = self.video.read()
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (48, 48))
+            emotion_pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
